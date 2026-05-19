e0144a4ef47d9c8b5765bd833693ccb57f565c34
shroommu <alexa.kruckenberg@gmail.com>
2026-05-19 12:00:54 -0600

    fetch all data

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,23 +12,56 @@ export default function Home() {
     loading: autoLoading,
     error: autoError,
   } = useJsonData("/data/auto_cleaned.json");
+  const {
+    data: creditCardData,
+    loading: creditCardLoading,
+    error: creditCardError,
+  } = useJsonData("/data/creditcard_cleaned.json");
+  const {
+    data: mortgageData,
+    loading: mortgageLoading,
+    error: mortgageError,
+  } = useJsonData("/data/mortgage_cleaned.json");
+  const {
+    data: studentLoanData,
+    loading: studentLoanLoading,
+    error: studentLoanError,
+  } = useJsonData("/data/studentloan_cleaned.json");
+  const {
+    data: totalData,
+    loading: totalLoading,
+    error: totalError,
+  } = useJsonData("/data/total_cleaned.json");
 
   const geoJsonRef = useRef(null);
+  const mapRef = useRef(null);
 
   const [activeDataFilters, setActiveDataFilters] = useState({
-    year: "2023",
     auto: true,
+    creditCard: true,
+    mortgage: true,
+    studentLoan: true,
+    total: true,
   });
 
-  const [mapColorActiveFilter, setMapColorActiveFilter] = useState("auto");
+  const [activeYear, setActiveYear] = useState("2025");
+
+  const [mapColorActiveFilter, setMapColorActiveFilter] = useState("total");
+
+  const dataMapping = {
+    auto: autoData,
+    creditCard: creditCardData,
+    mortgage: mortgageData,
+    studentLoan: studentLoanData,
+    total: totalData,
+  };
 
   const colorScaleMinMax = useMemo(() => {
-    if (mapColorActiveFilter === "auto") {
-      const values = Object.values(autoData || {}).map((d) => d["2023"]);
-      return [Math.min(...values.filter(Boolean)), Math.max(...values)];
-    }
-    return [0, 50000]; // default min and max values for color scale
-  }, [mapColorActiveFilter, autoLoading, autoData]);
+    const values = Object.values(dataMapping[mapColorActiveFilter] || {}).map(
+      (d) => d[activeYear],
+    );
+    return [Math.min(...values.filter(Boolean)), Math.max(...values)];
+  }, [mapColorActiveFilter, dataMapping, mapColorActiveFilter, activeYear]);
 
   const colorScale = (value: number, max: number) => {
     const scale = [
@@ -39,7 +72,7 @@ export default function Home() {
       colorScaleMinMax[0] + (colorScaleMinMax[1] - colorScaleMinMax[0]) * 0.8,
       colorScaleMinMax[1],
     ];
-    return value > scale[5]
+    return value >= scale[5]
       ? "#bd0026"
       : value > scale[4]
         ? "#f03b20"
@@ -49,19 +82,21 @@ export default function Home() {
             ? "#feb24c"
             : value > scale[1]
               ? "#fed976"
-              : value > scale[0]
+              : value >= scale[0]
                 ? "#ffffb2"
                 : "grey";
   };
 
   const geoJsonStyle = (feature) => ({
     fillColor: colorScale(
-      autoData
-        ? autoData[stateAbbvMapping[feature.properties.name]]["2023"]
+      dataMapping[mapColorActiveFilter]
+        ? dataMapping[mapColorActiveFilter][
+            stateAbbvMapping[feature.properties.name]
+          ][activeYear]
         : 0,
       50000,
     ),
-    fillOpacity: 0.7,
+    fillOpacity: 0.8,
     color: "black",
     weight: 1,
   });
@@ -69,21 +104,20 @@ export default function Home() {
   const onEachFeatureHandler = (feature: any, layer: any) => {
     const stateName = feature.properties.name;
     const stateAbbv = stateAbbvMapping[stateName];
-    const autoInfo = autoData ? autoData[stateAbbv]["2023"] : null;
+    const autoInfo = autoData ? autoData[stateAbbv][activeYear] : null;
+    const totalDebtInfo = totalData ? totalData[stateAbbv][activeYear] : null;
 
     layer.on({
-      click: () =>
-        console.log(
-          `clicked on ${stateName} (${stateAbbv}) - Auto Info: ${autoInfo}`,
-        ),
+      click: (e) => mapRef.current?.fitBounds(e.target.getBounds()),
       pointerover: () => layer.setStyle({ fillOpacity: 1, weight: 2 }),
       pointerout: () => geoJsonRef.current?.resetStyle(),
     });
 
     layer.bindTooltip(
       `<div>
-        <p>${stateName} (${stateAbbv})</p>
-        <p>Auto Debt Per Capita: $${autoInfo}.00</p>
+        <p><b>${stateName}</b></p>
+        <p>Auto Debt Per Capita: $${autoInfo.toLocaleString()}</p>
+        <p>Total Debt Per Capita: $${totalDebtInfo.toLocaleString()}</p>
       </div>`,
       { direction: "top", opacity: 1 },
     );
@@ -91,6 +125,7 @@ export default function Home() {
 
   return (
     <MapContainer
+      ref={mapRef}
       center={[39.8097343, -98.5556199]}
       zoom={4}
       style={{ height: "100vh", width: "100%" }}
@@ -99,7 +134,7 @@ export default function Home() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {!autoLoading && (
+      {!totalLoading && (
         <GeoJSON
           data={statesData}
           ref={geoJsonRef}
