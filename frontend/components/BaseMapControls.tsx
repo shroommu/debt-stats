2890cd927adca252e94245112b213019ca1f6c02
shroommu <alexa.kruckenberg@gmail.com>
2026-05-21 14:57:45 -0600

    remove nested transition

--- frontend/components/BaseMapControls.tsx
+++ frontend/components/BaseMapControls.tsx
@@ -88,137 +88,135 @@ export default function BaseMapControls({
           }}
         />
       </Fade>
-      <Fade in={hideControls} timeout={500}>
-        <Grow
-          in={showFilters}
-          timeout="auto"
-          style={{ transformOrigin: "bottom right 0" }}
+      <Grow
+        in={showFilters && hideControls}
+        timeout="auto"
+        style={{ transformOrigin: "bottom right 0" }}
+      >
+        <Box
+          component="div"
+          sx={{
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            zIndex: 999,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            p: 2,
+            backgroundColor: "white",
+            borderRadius: 4,
+            boxShadow: 3,
+          }}
         >
-          <Box
-            component="div"
-            sx={{
-              position: "absolute",
-              bottom: 20,
-              right: 20,
-              zIndex: 999,
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-              p: 2,
-              backgroundColor: "white",
-              borderRadius: 4,
-              boxShadow: 3,
-            }}
-          >
-            <Box component="div">
-              <Typography variant="h6" sx={{ fontSize: "1rem" }}>
-                Debt Type
-              </Typography>
-              <Select
-                size="small"
-                value={mapColorActiveFilter}
-                onChange={(e) => {
-                  setMapColorActiveFilter(e.target.value);
-                  const allFalse = Object.keys(activeDataFilters).reduce(
-                    (acc: { [key: string]: boolean }, key) => {
-                      acc[key] = false;
-                      return acc;
-                    },
-                    {},
-                  );
-                  const newFilters = {
-                    [e.target.value]: true,
-                  };
-                  setActiveDataFilters({ ...allFalse, ...newFilters });
-                  setGeoJsonRefreshKey(Math.random());
-                }}
-                style={{ width: "100%" }}
-              >
-                {Object.keys(dataMapping).map((key) => (
-                  <MenuItem key={key} value={key}>
-                    {dataLabels[key as keyof typeof dataLabels]}
-                  </MenuItem>
-                ))}
-              </Select>
-            </Box>
-            <Box component="div">
-              <Typography variant="h6" sx={{ fontSize: "1rem" }}>
-                Year
-              </Typography>
-              <Select
-                size="small"
-                value={activeYear}
-                onChange={(e) => {
-                  setActiveYear(e.target.value);
-                  setGeoJsonRefreshKey(Math.random());
-                }}
-                style={{ width: "100%" }}
-                MenuProps={{
-                  anchorOrigin: {
-                    vertical: "top",
-                    horizontal: "center",
+          <Box component="div">
+            <Typography variant="h6" sx={{ fontSize: "1rem" }}>
+              Debt Type
+            </Typography>
+            <Select
+              size="small"
+              value={mapColorActiveFilter}
+              onChange={(e) => {
+                setMapColorActiveFilter(e.target.value);
+                const allFalse = Object.keys(activeDataFilters).reduce(
+                  (acc: { [key: string]: boolean }, key) => {
+                    acc[key] = false;
+                    return acc;
                   },
-                  transformOrigin: {
-                    vertical: "bottom",
-                    horizontal: "center",
-                  },
-                }}
-              >
-                {[
-                  "2015",
-                  "2016",
-                  "2017",
-                  "2018",
-                  "2019",
-                  "2020",
-                  "2021",
-                  "2022",
-                  "2023",
-                  "2024",
-                  "2025",
-                ].map((year) => (
-                  <MenuItem key={year} value={year}>
-                    {year}
-                  </MenuItem>
-                ))}
-              </Select>
-            </Box>
-            <Box component="div">
-              <Typography variant="h6" sx={{ fontSize: "1rem" }}>
-                Show Debt Detail
-              </Typography>
-              {Object.keys(activeDataFilters).map((key) => (
-                <Box
-                  component="div"
-                  key={key}
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <Checkbox
-                    size="small"
-                    checked={
-                      activeDataFilters[key as keyof typeof activeDataFilters]
-                    }
-                    onChange={() => {
-                      setActiveDataFilters({
-                        ...activeDataFilters,
-                        [key]:
-                          !activeDataFilters[
-                            key as keyof typeof activeDataFilters
-                          ],
-                      });
-                      setGeoJsonRefreshKey(Math.random());
-                    }}
-                    sx={{ p: 0.5, pl: 0 }}
-                  />
-                  <Typography sx={{ marginLeft: "5px", fontSize: "0.875rem" }}>
-                    {dataLabels[key as keyof typeof dataLabels]}
-                  </Typography>
-                </Box>
+                  {},
+                );
+                const newFilters = {
+                  [e.target.value]: true,
+                };
+                setActiveDataFilters({ ...allFalse, ...newFilters });
+                setGeoJsonRefreshKey(Math.random());
+              }}
+              style={{ width: "100%" }}
+            >
+              {Object.keys(dataMapping).map((key) => (
+                <MenuItem key={key} value={key}>
+                  {dataLabels[key as keyof typeof dataLabels]}
+                </MenuItem>
               ))}
-            </Box>
+            </Select>
           </Box>
-        </Grow>
-      </Fade>
+          <Box component="div">
+            <Typography variant="h6" sx={{ fontSize: "1rem" }}>
+              Year
+            </Typography>
+            <Select
+              size="small"
+              value={activeYear}
+              onChange={(e) => {
+                setActiveYear(e.target.value);
+                setGeoJsonRefreshKey(Math.random());
+              }}
+              style={{ width: "100%" }}
+              MenuProps={{
+                anchorOrigin: {
+                  vertical: "top",
+                  horizontal: "center",
+                },
+                transformOrigin: {
+                  vertical: "bottom",
+                  horizontal: "center",
+                },
+              }}
+            >
+              {[
+                "2015",
+                "2016",
+                "2017",
+                "2018",
+                "2019",
+                "2020",
+                "2021",
+                "2022",
+                "2023",
+                "2024",
+                "2025",
+              ].map((year) => (
+                <MenuItem key={year} value={year}>
+                  {year}
+                </MenuItem>
+              ))}
+            </Select>
+          </Box>
+          <Box component="div">
+            <Typography variant="h6" sx={{ fontSize: "1rem" }}>
+              Show Debt Detail
+            </Typography>
+            {Object.keys(activeDataFilters).map((key) => (
+              <Box
+                component="div"
+                key={key}
+                sx={{ display: "flex", alignItems: "center" }}
+              >
+                <Checkbox
+                  size="small"
+                  checked={
+                    activeDataFilters[key as keyof typeof activeDataFilters]
+                  }
+                  onChange={() => {
+                    setActiveDataFilters({
+                      ...activeDataFilters,
+                      [key]:
+                        !activeDataFilters[
+                          key as keyof typeof activeDataFilters
+                        ],
+                    });
+                    setGeoJsonRefreshKey(Math.random());
+                  }}
+                  sx={{ p: 0.5, pl: 0 }}
+                />
+                <Typography sx={{ marginLeft: "5px", fontSize: "0.875rem" }}>
+                  {dataLabels[key as keyof typeof dataLabels]}
+                </Typography>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Grow>
       <Fade in={hideControls} timeout={500}>
         <Box
           component="div"
