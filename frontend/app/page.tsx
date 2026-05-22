2266081a4cbe7a5840d1fd0c382cf16dc101b4e4
shroommu <alexa.kruckenberg@gmail.com>
2026-05-22 17:50:51 -0600

    w -> dvw

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -184,7 +184,7 @@ export default function Home() {
   };
 
   return (
-    <Box component="div" sx={{ height: "100dvh", width: "100%" }}>
+    <Box component="div" sx={{ height: "100dvh", width: "100dvw" }}>
       <BaseMapControls
         colorScaleLegendValues={colorScaleLegendValues}
         dataLabels={dataLabels}
